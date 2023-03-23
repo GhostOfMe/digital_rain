6f6ab48d99229c1f4232acc86b697d7dffc18f68
JQ <nail.prophet@gmail.com>
2023-03-23 13:32:12 +0200

    fix formatting

--- src/main.rs
+++ src/main.rs
@@ -29,8 +29,7 @@ fn main() {
                 .short("b")
                 .long("background")
                 .takes_value(true)
-                .help("Use the chosen background color. Improves color blending. Ex.: <--background=#2f3b35>"),
-        )
+                .help("Use the chosen background color. Improves color blending. Ex.: <--background=#2f3b35>"))
         .get_matches();
 
     let color: Option<(i16, i16, i16)> = match app.value_of("color") {
