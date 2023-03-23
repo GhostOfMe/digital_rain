5a55a0d22f3b4fee41a92cbfd0d578eb5dd2eade
JQ <nail.prophet@gmail.com>
2023-03-23 12:12:59 +0200

    fix help message

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ fn main() {
                 .short("b")
                 .long("background")
                 .takes_value(true)
-                .help("Use the chosen background color. Improves color blending. Ex.: <--background=#001020>"),
+                .help("Use the chosen background color. Improves color blending. Ex.: <--background=#2f3b35>"),
         )
         .get_matches();
 
