e9201aefc7e24ae682c115f39a3e3958d4beb280
JQ <nail.prophet@gmail.com>
2023-04-29 16:50:33 +0200

    fix default background value

--- src/ui.rs
+++ src/ui.rs
@@ -29,7 +29,7 @@ impl Config {
             },
             background: Color {
                 r: 0,
-                g: 255,
+                g: 0,
                 b: 0,
                 a: 1.0,
             },
